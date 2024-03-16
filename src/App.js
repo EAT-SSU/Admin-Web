7854daf4dcc5279e9d28f0f9aa32e6ac829e53bb
choiseohyeon <csh200310@naver.com>
2024-03-16 11:03:55 +0900

    [chore] index.html 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login/Login';
-import Manage from './components/Manage/Manage';
-import { ThemeProvider } from '@mui/material/styles';
-import GlobalStyle from './styles/globalStyle';
-import { theme } from './styles/theme';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login/Login";
+import Manage from "./components/Manage/Manage";
+import { ThemeProvider } from "@mui/material/styles";
+import GlobalStyle from "./styles/globalStyle";
+import { theme } from "./styles/theme";
 
 function App() {
   return (
